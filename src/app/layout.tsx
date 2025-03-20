f338ecf9fc64caa3aff66bec0f3b81671d036c6b
Naoki_Ito <117070296+naoki-00-ito@users.noreply.github.com>
2025-03-20 13:12:17 +0900

    🐞 fix: layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <header className="sticky top-0 z-10  shadow">
+          <div className="container mx-auto p-4">
+            <h1 className="text-3xl font-bold">Debug Request</h1>
+          </div>
+        </header>
+        <main className="container mx-auto px-4 mt-4">
+          {children}
+        </main>
       </body>
     </html>
   );
